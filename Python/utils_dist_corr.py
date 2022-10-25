d351855ee44e039438325838f4ba0ca924e73e6d
Anne-Lise Saive <97337223+annelisesaive@users.noreply.github.com>
2022-10-25 14:53:12 +0100

    phys - percept spaces

--- Python/utils_dist_corr.py
+++ Python/utils_dist_corr.py
@@ -3,6 +3,14 @@ import numpy as np
 from scipy import stats
 from itertools import product, combinations
 
+subjects_no_out = ['S29-SBC', 'S17-CSB', 'S18b-SBC', 'S03-BSC', 'S25-BCS', 'S17-BCS', 'S01-BCS',
+                        'S24-SCB', 'S11-SBC', 'S01-BSC', 'S30-SCB', 'S12-CBS', 'S31-CSB', 'S23-SBC',
+                        'S11b-SBC', 'S13-BCS', 'S05-BCS', 'S20-SCB', 'S14-CBS', 'S12-SCB', 'S04-SCB',
+                        'S07-BSC', 'S07-BCS', 'S15-SBC', 'S19-BCS', 'S26-BSC', 'S08-CSB', 'S19-CBS',
+                        'S16-CSB', 'S08-BSC', 'S18-SBC', 'S02-CSB', 'S16-BSC', 'S21-CBS', 'S27-CBS',
+                        'S13-SCB', 'S28-CSB', 'S15-CBS', 'S03-CBS', 'S09-CBS', 'S21b-CBS', 'S06-CSB',
+                        'S10-CSB', 'S33-CBS', 'S23-SCB', 'S20b-SCB', 'S22-CSB', 'S10-BCS']
+
 def nb_pairs(n):
     """
     compute nb of potential pairs in an n dataset
@@ -24,7 +32,7 @@ def define_pairs_diff(names):
     all_pairs = [(p0, p1) for p0, p1 in combinations(names,2)]
     pairs = [p for p in all_pairs if p[0][0] != p[1][0]]
     return pairs
-    
+
 def compute_distance(A_xy, B_xy):
     """
     compute euclidian distance btw 2 points (A & B) using 2D coordinates
